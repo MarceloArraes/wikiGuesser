bcdd1c750daf2a7d8335dbd26945bc6fdd4a23aa
MarceloArraes <marcelo.arraes@gmail.com>
2022-05-28 19:40:15 +0000

    starting understanding the wikipedia API

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+//This site: https://en.wikipedia.org/wiki/Special:ApiSandbox
+//action=query = revisions, pageassesments, pageviews
+//proprevisions = content
+//generator = random,  grnnamespace = (article), grnlimit=10.
+//format json.
+//https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions%7Cpageassessments%7Cpageviews&indexpageids=1&generator=random&formatversion=2&rvprop=content&grnnamespace=0&grnfilterredir=nonredirects&grnlimit=10
+
 
 export default function App() {
   return (
