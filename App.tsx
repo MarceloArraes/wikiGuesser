be6a5908b01c649c5c4a440ba4d920fa424ff2e1
MarceloArraes <marcelo.arraes@gmail.com>
2022-05-29 23:38:46 +0000

    feat: add navigation 6x and stack.navigator

--- App.tsx
+++ App.tsx
@@ -1,27 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+
 //This site: https://en.wikipedia.org/wiki/Special:ApiSandbox
 //action=query = revisions, pageassesments, pageviews
 //proprevisions = content
 //generator = random,  grnnamespace = (article), grnlimit=10.
 //format json.
-//https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions%7Cpageassessments%7Cpageviews&indexpageids=1&generator=random&formatversion=2&rvprop=content&grnnamespace=0&grnfilterredir=nonredirects&grnlimit=10
+//https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions%7Cpageassessments%7Cpageviews&indexpageids=1&generator=random&formatversion=2&rvprop=content%7Ccontentmodel%7Coresscores%7Croles&grnnamespace=0&grnfilterredir=nonredirects&grnlimit=10
 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 20,
+}});
+
+
+export default HomeScreen
